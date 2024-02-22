f1e122b9097153829f59eb380f0b496ed386a9bf
itsAman-24 <amanpan2410@gmail.com>
2024-02-22 22:23:56 +0530

    Added

---
+++ Even_1-100.js
@@ -0,0 +1,11 @@
+for(let num = 1; num <= 100; num++) {
+    //EVEN Number
+    if(num % 2 === 0) {
+    console.log("Even Num =",num);
+    }
+
+    //ODD Number
+    if(num % 2 !== 0) {
+        console.log("Odd Num =",num);
+    }
+}
