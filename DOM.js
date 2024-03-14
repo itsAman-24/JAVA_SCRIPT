fae6fa1a38d10d312ba3f681bc245635edff35ea
itsAman-24 <amanpan2410@gmail.com>
2024-03-14 21:55:08 +0530

    Added

--- DOM.js
+++ DOM.js
@@ -9,25 +9,28 @@
 
 //By using Id 
 let demo = document.getElementById("h2");
-console.dir(demo);
-console.log(demo);
+// console.dir(demo);
+// console.log(demo);
 
 //By using className
 let demo2 = document.getElementsByClassName("header");
-console.dir(demo2);
-console.log(demo2);
+// console.dir(demo2);
+// console.log(demo2);
 
 //By using tagName
 let demo3 = document.getElementsByTagName("p");
-console.dir(demo3);
-console.log(demo3);
+// console.dir(demo3);
+// console.log(demo3);
 
 //By using querySelectors
 /*we can provide anything in querySelectors idName / className / tagName it will return first matching element */
 let demo4 = document.querySelector("p");
-console.dir(demo4);
+// console.dir(demo4);
 
 //querySelectorAll will return nodeList of every matching element
 
 let demo5 = document.querySelectorAll("#h2");
-console.dir(demo5);
+// console.dir(demo5);
+
+let divTag = document.querySelector("div");
+console.dir(divTag);
