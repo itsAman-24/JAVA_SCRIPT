e0d07365cc9055703136fd5dd784a863c72c5a2a
itsAman-24 <amanpan2410@gmail.com>
2024-03-21 22:25:10 +0530

    Added

--- DOM_Attribute.js
+++ DOM_Attribute.js
@@ -1,2 +1,16 @@
 let div = document.querySelector("div");
-console.log(div);
+console.log(div.getAttribute("id" , "class"));
+
+console.log( div.setAttribute("class" , "newClass") );   //set the class attribute as newClass
+
+console.log( div.setAttribute("id" , "newId") );   //set the id attribute as newiD
+
+div.style.backgroundColor = "red";  //setting css properties by using javascript 
+
+div.style.textAlign = "center";
+
+div.style.fontSize = "2rem";
+
+div.innerText = "Hi Coders";
+
+console.log(div.innerHTML);
