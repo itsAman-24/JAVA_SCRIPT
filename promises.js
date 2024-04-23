d4c8b17c617a664d80ad52fcd74242cbc3abeb9b
itsAman-24 <amanpan2410@gmail.com>
2024-04-23 22:17:59 +0530

    Added

--- promises.js
+++ promises.js
@@ -18,7 +18,7 @@ const getId = createOrderId (carts , (orderId) => {
 
 //AFTER USING PROMISES OBJECTS the creatOrderId() has no control over callback function which is proceedToPayment() ,, as we are using PROMISES here to store the returned value from the creatOrderId() aftet getting the returned value form the creatOrderId() PROMISES.then() will be called which is totally independent from the creatOrderId(); by this PROMISES helps to control the callback functions by the developers itself 
 
-const promises = createOrderId(carts);
+const promises = createOrderId(carts);  // createOrderId() function will return orderId which will be stored inside promise object
 
 promises.then( function (orderId) {      //then() is executed after the data get by the promise object as a returned value 
     proceedToPayment(orderId);
