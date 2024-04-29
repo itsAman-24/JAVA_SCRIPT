bacc935d4a69668e9b95eefc73753869fdcda4ef
itsAman-24 <amanpan2410@gmail.com>
2024-04-29 21:32:37 +0530

    Added

--- eCommerceDummy.js
+++ eCommerceDummy.js
@@ -1,90 +1,101 @@
-const cart = ["shoes" , "shirts" , "jeans"];
+const cart = ["shoes", "shirts", "jeans"];
 
 createOrderId(cart)
-.then(function(orderId) {
-    console.log(orderId);
-    return orderId;
-})
-
-.then(function(orderId) {
-    return proceedToPayment(orderId);
-})
-
-.then(function paymentDone(payment) {
-    console.log("Payement done");
-    return payment;
-})
-
-.then(function(order) {
-    return orderSummary(order);
-})
-
-.then(function(amount) {
-    return updateWallet(amount);
-})
-
-.then(function(walletInfo) {
-    return walletBalance(walletInfo);
-})
+    .then(function(orderId) {
+        console.log(orderId);
+        return orderId;
+    })
+    .then(function(orderId) {
+        return proceedToPayment(orderId);
+    })
+    .then(function(payment) {
+        console.log("Payment done");
+        return payment;
+    })
+    .then(function(order) {
+        console.log(order);
+        return orderSummary(order);
+    })
+    .then(function(amount) {
+        console.log(amount);
+        return updateWallet(amount);
+    })
+    .then(function(walletInfo) {
+        console.log("Wallet updated");
+        return walletBalance(walletInfo);
+    })
+    .catch(function(error) {
+        console.error("Error:", error);
+    });
 
 function proceedToPayment(orderId) {
-    return new Promise(function(reject,resolve) {
+    return new Promise(function(resolve, reject) {
         setTimeout(function() {
-            if(orderId) {
+            if (orderId) {
                 console.log("Payment successfully done");
                 resolve(orderId);
-            }
-            else {
+            } else {
                 const err = new Error("Error while payment");
                 reject(err);
             }
-        },4000);
-    })
-};
+        }, 1000);
+    });
+}
 
 function orderSummary(order) {
-    return new Promise(function(reject,resolve) {
+    return new Promise(function(resolve, reject) {
         setTimeout(function() {
-            if(order) {
+            if (order) {
                 console.log("Order is placed successfully");
                 resolve(order);
-            }
-            else {
+            } else {
                 const err = new Error("Order is not placed");
                 reject(err);
             }
-        },4000);
-    })
+        }, 2000);
+    });
 }
 
 function updateWallet(amount) {
-    return new Promise(function(reject,resolve) {
+    return new Promise(function(resolve, reject) {
         setTimeout(function() {
-            if(amount) {
+            if (amount) {
                 console.log("Amount paid");
                 resolve(amount);
-            }
-
-            else {
-                const err  = new Error("Amount is pending");
+            } else {
+                const err = new Error("Amount is pending");
                 reject(err);
             }
-        },5000);
-    })
+        }, 1000);
+    });
 }
 
-function updateWallet(walletInfo) {
-    return new Promise(function(resolve,reject) {
+function walletBalance(amount) {
+    return new Promise(function(resolve, reject) {
         setTimeout(function() {
-            if(updateWallet) {
-                console.log("Wallet Upadted");
-                resolve(walletInfo);
-            }
-
-            else {
-                const err = new Error("Error in updating");
+            if (amount) {
+                console.log("Wallet updated");
+                resolve(amount);
+            } else {
+                const err = new Error("Error in updating wallet amount");
                 reject(err);
             }
-        },4000);
-    })
+        }, 1000);
+    });
+}
+
+function createOrderId(cart) {
+    //creating promise
+    return new Promise(function(resolve, reject) {
+        if (!verifyOrderId(cart)) {
+            const error = new Error("There is some problem");
+            reject(error);
+        } else {
+            resolve(cart);
+        }
+    });
+}
+
+function verifyOrderId(cart) {
+    return true;
 }
