43bdd8d09f4b71ad9f76830ec8ab7b8ac0f0e867
itsAman-24 <amanpan2410@gmail.com>
2024-05-02 22:06:14 +0530

    Added

---
+++ async.js
@@ -0,0 +1,12 @@
+// getData = async() => {
+//     return "randomValue";
+// }
+
+// const Value = getData;
+// console.log(Value);
+
+async function getData() {
+    return "randomValue";
+}
+
+const value = getData();
