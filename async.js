7ddd8ec6f809932841e49c783022062a2f428664
itsAman-24 <amanpan2410@gmail.com>
2024-05-06 21:30:03 +0530

    Added

--- async.js
+++ async.js
@@ -1,30 +1,62 @@
-// getData = async() => {
-//     return "randomValue";
+// // getData = async() => {
+// //     return "randomValue";
+// // }
+
+// // const Value = getData;
+// // console.log(Value);
+
+
+// // This function will return promise 
+// async function getData() {
+//     setTimeout(() => {
+//         return "NamsteJavaScript";
+//     } ,2000)
+
+//     // return "Nmaste";
 // }
 
-// const Value = getData;
-// console.log(Value);
+// const dataPromise = getData();
+// debugger
+// console.log(dataPromise);
+// debugger
+
+// dataPromise.then((Response) => { console.log(Response); })
 
 
-// This function will return promise 
-async function getData() {
-    setTimeout(() => {
-        return "NamsteJavaScript";
-    } ,2000)
 
-    // return "Nmaste";
+// //Await keyword is only used with async function
+
+
+// function getData(callback) {
+//     // debugger
+//     setTimeout(() => {
+//         // callback("NamasteJavaScript");
+//         callback("Namstey");
+//     }, 2000);
+//     // debugger
+// }
+
+// const callback = (strValue) => {
+//     return "Namaste";
+// }
+
+// // Usage
+// getData(data => {
+//     // debugger
+//     console.log(data); // Output: NamasteJavaScript
+// });
+
+
+function getData2() {
+    return new Promise( (reject , resolve) => {
+        setTimeout(() => {
+            resolve("Namastey");
+        },4000);
+
+        
+    })
 }
 
-const dataPromise = getData();
-debugger
-console.log(dataPromise);
-debugger
-
-dataPromise.then((Response) => { console.log(Response); })
-
-
-
-//Await keyword is only used with async function
-
-
-
+getData2().then(data => {
+    console.log(data);
+})
